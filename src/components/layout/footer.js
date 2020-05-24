05130511a1f139c7ca13aa0630aa29027b17b576
Andrew Schwartz <andrew@aschwartz.me>
2020-05-24 13:45:35 -0600

    chore: capitalization for footer title

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
   <footer>
-    <h3>Contact Me</h3>
+    <h3>Contact me</h3>
     <p>Source + hosting on Github</p>
     <p>Made with ♥ and Gatsby in Colorado, USA</p>
     <p>© Andrew Schwartz 2020</p>
