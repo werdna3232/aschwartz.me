d813d21066563ad979e800142c466993731d32db
Andrew Schwartz <andrew@aschwartz.me>
2020-05-24 13:06:51 -0600

    feat: add item styles

--- src/components/item.js
+++ src/components/item.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import ItemStyles from './item.module.scss'
 
 export default ({ title, content, url }) => (
   <div>
     {url ? (
-      <h3>{title}</h3>
+      <h3 className={ItemStyles.title}>{title}</h3>
     ) : (
-      <h3>
+      <h3 className={ItemStyles.title}>
         <a href={url}>{title}</a>
       </h3>
     )}
-    <p>{content}</p>
+    <p className={ItemStyles.content}>{content}</p>
   </div>
 )
