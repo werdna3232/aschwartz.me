d44ebafc07ab730834c7e8df090de1380b430497
Andrew Schwartz <andrew@aschwartz.me>
2020-05-24 13:17:00 -0600

    chore: add lede component

---
+++ src/components/lede.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import LedeStyles from './lede.module.scss'
+
+export default ({ children }) => <h2 className={LedeStyles.lede}>{children}</h2>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,12 +3,17 @@ import React from 'react'
 import Layout from '../components/layout'
 import Header from '../components/header'
 import Section from '../components/section'
+import Lede from '../components/lede'
 
 import ProjectsData from '../../content/projects.yaml'
 
 export default () => (
   <Layout title="Projects">
     <Header />
+    <Lede>
+      Explore what I’ve been working on below ↓ or on{' '}
+      <a href="github.com/schwartzadev/">GitHub</a>.
+    </Lede>
     <Section items={ProjectsData.projects} />
   </Layout>
 )
