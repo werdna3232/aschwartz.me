e673699cdc5af084a04ade7f79a37a355410dd6d
Andrew Schwartz <andrew@aschwartz.me>
2020-05-24 13:52:45 -0600

    chore: add links to footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,8 +3,14 @@ import React from 'react'
 export default () => (
   <footer>
     <h3>Contact me</h3>
-    <p>Source + hosting on Github</p>
-    <p>Made with ♥ and Gatsby in Colorado, USA</p>
+    <p>
+      Source + hosting on{' '}
+      <a href="https://github.com/schwartzadev/aschwartz.me">GitHub</a>
+    </p>
+    <p>
+      Made with ♥ and <a href="https://www.gatsbyjs.org/">GatsbyJS</a> in
+      Colorado, USA
+    </p>
     <p>© Andrew Schwartz 2020</p>
   </footer>
 )
